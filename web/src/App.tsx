021f357fb9534924889560ae7a7239614f151ad1
Kyle Overstreet <KOverstreet@grimmway.com>
2021-11-18 16:57:30 -0800

    wip frontend asset tags

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,14 +1,18 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import NavBar from "./components/NavBar";
+import Asset from "./pages/Asset";
 import Overview from "./pages/Overview";
+import AssetTags from "./pages/AssetTags";
 
 function App() {
   return (
     <BrowserRouter>
       <NavBar loggedIn={false} onLoginClick={() => {}} onLogoutClick={() => {}} />
       <Routes>
-        <Route path="/" element={<Overview />} />
+        {/* <Route path="/" element={<Overview />} /> */}
+        <Route path="/" element={<Asset />} />
+        <Route path="/tags" element={<AssetTags />} />
       </Routes>
     </BrowserRouter>
   );
