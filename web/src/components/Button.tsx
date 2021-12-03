98925f3ea35e2457ec6d50b9c7f07b2010c39553
Kyle Overstreet <KOverstreet@grimmway.com>
2021-12-03 15:29:36 -0800

    added button component

---
+++ web/src/components/Button.tsx
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+
+export interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  className?: string;
+}
+
+const Button: React.FC<ButtonProps> = (props: ButtonProps) => {
+  console.log("Button evaluated.");
+
+  const [bgColor, setBgColor] = useState("#37c1d3");
+
+  const styles: any = {
+    color: "black",
+    //background: "#53d337",
+    background: bgColor,
+    border: "0px",
+    borderRadius: "5px",
+    cursor: "pointer",
+    width: "70px",
+    height: "35px",
+    fontWeight: "bold",
+  };
+
+  return (
+    <button
+      {...props}
+      style={styles}
+      onMouseEnter={() => setBgColor("#2c8e9b")}
+      onMouseLeave={() => setBgColor("#2e9caa")}
+    >
+      {props.children}
+    </button>
+  );
+};
+
+export default Button;
