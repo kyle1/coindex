09e28fd345321970e75fef53e5fa5d7a3eaf2be2
Kyle Overstreet <KOverstreet@grimmway.com>
2021-12-02 16:54:35 -0800

    wip frontend

--- web/src/models/Asset.ts
+++ web/src/models/Asset.ts
@@ -1,8 +1,14 @@
+import { AssetTag } from "./AssetTag";
+
 export interface Asset {
   assetId: number;
   assetName: string;
   ticker: string;
   website?: string;
   subreddit?: string;
+  transactionsPerSecond?: string;
   mentions?: number;
+
+  tags?: AssetTag[];
+  competitors?: Asset[];
 }
